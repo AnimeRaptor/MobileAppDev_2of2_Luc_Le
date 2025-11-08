903a74b9fdcf4598e9dae1a5abaeec1a16f98e16
AnimeRaptor <westernboy2004@gmail.com>
2025-11-08 06:06:58 +0000

    Yee

--- EHLL/App.js
+++ EHLL/App.js
@@ -1,9 +1,35 @@
 import * as React from 'react';
-import { createStaticNavigation } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
+import { Button } from '@react-navigation/elements';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen.js';
+function HomeScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('Profile')}>
+        Go to Profile
+      </Button>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
+      <Button onPress={() => navigation.navigate('Home')}>Go to Home</Button>
+    </View>
+  );
+}
 
 const MyTabs = createBottomTabNavigator({
   screens: {
@@ -17,4 +43,5 @@ const Navigation = createStaticNavigation(MyTabs);
 export default function App() {
   return <Navigation />;
 }
+
 
